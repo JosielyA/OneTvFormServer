07385ece812028763c4adf4517ed4263d2da488d
Josiely <josiely.araujo21@gmail.com>
2024-03-07 19:15:13 -0400

    cors 1

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.text());
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
