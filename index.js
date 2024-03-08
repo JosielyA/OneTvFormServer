ba619e5574448b593648c7eb3cac205605447573
Josiely <josiely.araujo21@gmail.com>
2024-03-07 21:23:23 -0400

    server to index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.text());
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
