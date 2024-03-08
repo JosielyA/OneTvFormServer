3700c5c09b77be17f9df8e32626af99ff8468ebf
Josiely <josiely.araujo21@gmail.com>
2024-03-07 21:17:30 -0400

    get

--- server.js
+++ server.js
@@ -7,14 +7,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(express.text());
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
@@ -67,6 +60,10 @@ app.post("/api/formulario", async (req, res) => {
   } catch (error) {
     res.json({ message: "bad", error });
   }
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "good" });
 });
 
 app.listen(port);
